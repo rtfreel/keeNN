6dbdf456637f8499fef4acd960caaae008b3dec1
Roman Tkachenko <rt4freel@gmail.com>
2020-08-15 01:35:29 +0300

    Added simple Gradle Java Library project structure

---
+++ src/main/java/keenn/Model.java
@@ -0,0 +1,8 @@
+package keenn;
+
+/**
+ * Model objects store neural networks itselves which are able to run and learn.
+ */
+public class Model {
+    
+}
---
+++ src/test/java/keenn/ModelTest.java
@@ -0,0 +1,11 @@
+package keenn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ModelTest {
+    @Test public void testSomeModelMethod() {
+        Model model = new Model();
+        assertTrue("model should not be null", model!=null);
+    }
+}
